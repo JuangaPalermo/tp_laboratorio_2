a35e0893a9ee8c488f118faa8a76275bb8818c6f
JuangaPalermo <juangabrielpalermo@gmail.com>
2020-10-29 20:13:27 -0300

    Se hacen test unitarios de Colecciones

---
+++ TPS_ProgYLabo2/TP3/Palermo.Juan.2A.TP3/Test Unitarios/TestColecciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clases_Instanciables;
+using EntidadesAbstractas;
+using Excepciones;
+using Archivos;
+
+namespace Test_Unitarios
+{
+    [TestClass]
+    public class TestColecciones
+    {
+        [TestMethod]
+        public void TestColeccionAlumnos()
+        {
+            Universidad universidad = new Universidad();
+
+            Assert.IsNotNull(universidad.Alumnos);
+        }
+
+        [TestMethod]
+        public void TestColeccionProfesores()
+        {
+            Universidad universidad = new Universidad();
+
+            Assert.IsNotNull(universidad.Instructores);
+        }
+
+        [TestMethod]
+        public void TestColeccionJornadas()
+        {
+            Universidad universidad = new Universidad();
+
+            Assert.IsNotNull(universidad.Jornadas);
+        }
+    }
+}
